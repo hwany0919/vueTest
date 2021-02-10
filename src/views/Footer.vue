6c738e3036517585e970d50dc651df0d2a8010e9
moose <away8909@gmail.com>
2021-02-10 16:48:45 +0900

    세번째 커밋

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="footer">
     <div>
-      Footer || 데이터 입력 || commit 테스트
+      <div>Footer || 데이터 입력 || commit 테스트</div>
+      <div>두번째 줄</div>
     </div>
   </div>
 </template>
