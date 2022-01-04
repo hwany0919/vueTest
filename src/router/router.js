e4d63bdf38b63af88cdbc6a57d1547ca10d8bda1
moose <obh4385@spotv.net>
2022-01-04 17:04:44 +0900

    [Dev] Google Login Rest API Test

--- src/router/router.js
+++ src/router/router.js
@@ -7,7 +7,7 @@ export default new Router({
   mode: "history",
   routes: [
     {
-      path: "/",
+      path: "",
       component: () => import(/* webpackChunkName: "main" */"@/views/MainView"),
       children: [
         {
@@ -38,6 +38,11 @@ export default new Router({
           ]
         }
       ]
+    },
+    {
+      path: "/oauth",
+      component: () => import(/* webpackChunkName: "oauth" */"@/views/account/Oauth"),
+      meta: { public: true }
     }
   ]
 })
---
+++ src/views/account/Oauth.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="oauth">
+    API 로그인 연동<br/>
+    {{$route.query}}
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log("query :: ", this.$route.query);
+    this.$router.replace("/");
+  }
+}
+</script>
+
+<style lang="less">
+.oauth {
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
